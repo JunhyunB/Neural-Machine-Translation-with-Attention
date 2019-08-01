97ddc5280e388d7baef1c975b5449c2ace00965a
Junhyun Bae <junhyun.bae.kr@gmail.com>
2019-08-01 21:44:02 +0900

    Complete dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -27,19 +27,17 @@ class CustomDataset(torchdata.Dataset):
         print("Tokenization Complete")
         print("Making Vocabulary...")
 
+        flatten = lambda x: [tkn for s in x for tkn in s]
+        unique_tokens = sorted(set(flatten(source+target)))
+        
         self.vocab_stoi = defaultdict()
         self.vocab_stoi['<pad>'] = 0
+        self.vocab_stoi['<unk>'] = 1
+        self.vocab_stoi['<sos>'] = 2
+        self.vocab_stoi['<eos>'] = 3
 
-        with open('data/tokenizer.vocab', 'r', encoding='utf-8') as v:
-            unique_tokens = []
-            i = 1
-            while True:
-                line = v.readline().strip().split('\t')
-                if len(line) != 2:
-                    break
-                else:
-                    self.vocab_stoi[line[0]] = i
-                    i += 1
+        for i, token in enumerate(unique_tokens, 4):
+            self.vocab_stoi[token] = i
 
         self.vocab_itos = [t for t, i in sorted([(token, index) for token, index in self.vocab_stoi.items()], key=lambda x:x[1])]
         source_numerical = [list(map(self.vocab_stoi.get, s)) for s in source]
@@ -66,4 +64,7 @@ class CustomDataset(torchdata.Dataset):
         SOURCE = [s+[self.pad_idx]*(self.src_max_len-len(s)) if len(s) < self.src_max_len else s[:self.src_max_len] for s in src_]
         TARGET = [s+[self.pad_idx]*(self.trg_max_len-len(s)) if len(s) < self.trg_max_len else s[:self.trg_max_len] for s in trg_]
 
+        '''print(SOURCE)
+        print(TARGET)'''
+        
         return torch.LongTensor(SOURCE), torch.LongTensor(TARGET)
