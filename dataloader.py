8a9a14975f77235b42656d2c9d8b4a1efcf4156e
Junhyun Bae <junhyun.bae.kr@gmail.com>
2019-08-01 20:11:13 +0900

    Complete dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -4,22 +4,66 @@ import torch.utils.data as torchdata
 from collections import defaultdict
 from torchtext import data
 
+
+# This is Google SentencePiece Tokenizer
+# https://github.com/google/sentencepiece
+import sentencepiece as spm
+sp = spm.SentencePieceProcessor()
+sp.Load('data/tokenizer.model')
+
 class CustomDataset(torchdata.Dataset):
-    def __init__(self, path=None):
+    def __init__(self, path='data/eng-fra.txt'):
         with open(path, 'r', encoding='utf-8') as f:
-            pass
-
+            source = []
+            target = []
+            while True:
+                line = f.readline().strip().split('\t')
+                if len(line) != 2:
+                    break
+                else:
+                    source.append(['<s>']+sp.EncodeAsPieces(line[0])+['</s>'])
+                    target.append(['<s>']+sp.EncodeAsPieces(line[1])+['</s>'])
         
+        print("Tokenization Complete")
+        print("Making Vocabulary...")
+
+        self.vocab_stoi = defaultdict()
+        self.vocab_stoi['<pad>'] = 0
+
+        with open('data/tokenizer.vocab', 'r', encoding='utf-8') as v:
+            unique_tokens = []
+            i = 1
+            while True:
+                line = v.readline().strip().split('\t')
+                if len(line) != 2:
+                    break
+                else:
+                    self.vocab_stoi[line[0]] = i
+                    i += 1
+
+        self.vocab_itos = [t for t, i in sorted([(token, index) for token, index in self.vocab_stoi.items()], key=lambda x:x[1])]
+        source_numerical = [list(map(self.vocab_stoi.get, s)) for s in source]
+        target_numerical = [list(map(self.vocab_stoi.get, s)) for s in target]
+
+        self.src = source_numerical
+        self.trg = target_numerical
+        self.pad_idx = self.vocab_stoi['<pad>']
+
+        self.src_max_len = max(len(s) for s in self.src)
+        self.trg_max_len = max(len(s) for s in self.trg)
+
     def __getitem__(self, index):
+        # return index data
+        return [self.src[index], self.trg[index]]
 
-        return 0
-
-    
     def __len__(self):
-
-        return 0
-
+        # length of data
+        return len(self.src)
     
     def custom_collate_fn(self, data):
-        
-        return 0
+        src_, trg_ = list(zip(*data))
+
+        SOURCE = [s+[self.pad_idx]*(self.src_max_len-len(s)) if len(s) < self.src_max_len else s[:self.src_max_len] for s in src_]
+        TARGET = [s+[self.pad_idx]*(self.trg_max_len-len(s)) if len(s) < self.trg_max_len else s[:self.trg_max_len] for s in trg_]
+
+        return torch.LongTensor(SOURCE), torch.LongTensor(TARGET)
